c69a78b5b30d3b30726a9ba6150fe8a7a908fca4
madhu <madhu.java.git@gmail.com>
2023-02-14 10:32:36 +0530

    view files

---
+++ JDBCCRUDWEBAPP/src/com/madhu/dto/Student.java
@@ -0,0 +1,35 @@
+ package com.madhu.dto;
+
+import java.io.Serializable;
+
+import sun.util.logging.resources.logging;
+
+public class Student implements Serializable {
+	private static final long serialVersionUID=1;
+private String name;
+private int age;
+private String address;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+@Override
+public String toString() {
+	return "Student [name=" + name + ", age=" + age + ", address=" + address + "]";
+}
+
+}
